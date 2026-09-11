5c4da2f972071d5c16eddc2944b850961c06a772
Dainer Ruiz Morelo <Dainerruiz8@gmail.com>
2026-09-11 15:04:06 -0500

    modulo services

---
+++ Backend_Postes_Melany/src/services/pedido_service.js
@@ -0,0 +1,26 @@
+const pedidoRepository = require('../repositories/pedido.repository');
+
+const pedidoService = {
+    
+    async obtenerTodos() {
+        const pedidos = await pedidoRepository.obtenerPedidos();
+        return pedidos;
+    },
+
+    async crear(datosPedido) {
+        if (!datosPedido.postre || !datosPedido.cantidad) {
+            throw new Error('Faltan datos obligatorios: el postre y la cantidad son requeridos.');
+        }
+
+        const nuevoPedido = {
+            id: Date.now(),
+            postre: datosPedido.postre,
+            cantidad: datosPedido.cantidad,
+            fecha: new Date().toISOString()
+        };
+        const pedidoGuardado = await pedidoRepository.guardarPedido(nuevoPedido);
+        
+        return pedidoGuardado;
+    }
+};
+module.exports = pedidoService;
